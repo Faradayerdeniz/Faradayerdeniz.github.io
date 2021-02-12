6dc4ce2d495e908c1149cc1a85130ea55d9a1996
Alexander Deniz Bolanos <elperrodefara@hotmail.com>
2021-02-12 17:47:03 +0000

    Gulp implementado y funcionando

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+//Función gulp de prueba para ver si funciona correctamente
+function defaultTask(cb) {
+    // place code for your default task here
+    console.log("probando gulp");
+    cb();
+  }
+  
+  exports.default = defaultTask
